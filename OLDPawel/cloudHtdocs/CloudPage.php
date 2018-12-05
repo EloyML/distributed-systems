0d48ea4d7c5f46df94e116fb42ffb5c505320f50
EloyML <eloyml.524@gmail.com>
2018-12-05 17:03:43 +0000

    Prelim Changes
    tests

--- OLDPawel/cloudHtdocs/CloudPage.php
+++ OLDPawel/cloudHtdocs/CloudPage.php
@@ -19,6 +19,12 @@
  </head>
  <body>
  
+ <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+ </form>
+
   <header>
     <div class="navBar">
 <!-- Place holder for our logo -->
